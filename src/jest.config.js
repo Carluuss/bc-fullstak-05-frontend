6d76958eaa0df57eb716fc6a760f41b14a274113
Carlos <128008806+Carluuss@users.noreply.github.com>
2024-10-29 17:21:56 -0300

    first commit

---
+++ src/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "jsdom",
+    transform: {
+      "^.+\\.tsx?$": "ts-jest",
+    },
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    moduleNameMapper: {
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+      },
+  };
